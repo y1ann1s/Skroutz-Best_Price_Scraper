e5c96945d2fb75f8834ae231afb9ac185c0297dc
y1ann1s <meletisjohn@hotmail.com>
2025-06-12 19:24:34 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from Skroutz.main_program import get_product_data
+
+app = FastAPI()
+
+class ScrapeRequest(BaseModel):
+    query: str
+
+@app.post("/scrape")
+def scrape(req: ScrapeRequest):
+    try:
+        result = get_product_data(req.query)
+        return {"success": True, "data": result}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
